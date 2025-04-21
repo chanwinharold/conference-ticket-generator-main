94811c4ac807c4c7ad321cb7800862a9cf13d821
chanwinharold <chanwinharold@gmail.com>
2025-04-21 16:37:40 +0200

    Optimisation et Amélioration du code

--- README.md
+++ README.md
@@ -29,8 +29,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Solution URL here](https://your-solution-url.com)
-- Live Site URL: [Live site URL here](https://your-live-site-url.com)
+- Solution URL: [Solution URL here](https://github.com/chanwinharold/conference-ticket-generator-main)
+- Live Site URL: [Live site URL here](https://conference-ticket-generator-main-two.vercel.app/)
 
 ### Built with
 
@@ -39,6 +39,8 @@ Users should be able to:
 - Flexbox
 - CSS Grid
 - Mobile-first workflow
+- Javascript
+- Accessibility practices
 
 ## Author
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
-let regexName = new RegExp("^[A-Za-z]+ [A-Za-z]+$")
+let regexName = new RegExp("^(?!\\s*$).+")
 let regexEmail = new RegExp("^[A-Za-z0-9]+@[a-z0-9]+\\.[a-z]+$")
 let regexGithub = new RegExp("^@[A-Za-z0-9]+$")
 
+let uploader = document.querySelector(".form__uploader-section__sub-info")
+let subInfo = document.querySelector(".form__uploader-section__sub-info")
+let nameErrorSpan = document.querySelector("#name + span")
+let emailErrorSpan = document.querySelector("#email + span")
+let githubErrorSpan = document.querySelector("#github + span")
 
 document.getElementById("submit-ticket").addEventListener("click", (event) => {
 
@@ -11,29 +16,30 @@ document.getElementById("submit-ticket").addEventListener("click", (event) => {
     const github = document.getElementById("github")
     let counter = 0
 
-    if (uploaded.files[0]) { counter++ }
-    if (!regexName.test(fullName.value)) {
-        document.querySelector("#name + span").style.display = "inline-block"
-        fullName.style.borderColor = "hsl(7, 71%, 60%)"
-    } else counter++
-    if (!regexEmail.test(email.value)) {
-        document.querySelector("#email + span").style.display = "inline-block"
-        email.style.borderColor = "hsl(7, 71%, 60%)"
-    } else counter++
-    if (!regexGithub.test(github.value)) {
-        document.querySelector("#github + span").style.display = "inline-block"
-        github.style.borderColor = "hsl(7, 71%, 60%)"
-    } else counter++
+    let handleError = (rule, targetElement, errorSpan) => {
+        if (!rule.test(targetElement.value)) {
+            errorSpan.style.display = "inline-block"
+            targetElement.style.borderColor = "hsl(7, 71%, 60%)"
+        } else counter++;
+    }
+
+    if (!uploaded.files[0]) {
+        uploader.innerText = "No file uploaded! Please upload a photo."
+        uploader.style.color = "hsl(7, 71%, 60%)"
+        subInfo.classList.add("content")
+    } else counter++;
+
+    handleError(regexName, fullName, nameErrorSpan)
+    handleError(regexEmail, email, emailErrorSpan)
+    handleError(regexGithub, github, githubErrorSpan)
 
     if (counter === 4) {
         let userInfos = {
             "fullName": fullName.value,
             "email": email.value,
-            "github": github.value
-        }
+            "github": github.value }
 
         userInfos = JSON.stringify(userInfos)
-
         localStorage.removeItem("userInfos")
         localStorage.setItem("userInfos", userInfos)
         event.target.href = "ticket.html"
@@ -42,13 +48,13 @@ document.getElementById("submit-ticket").addEventListener("click", (event) => {
 
 
 document.getElementById('uploader').addEventListener('change', function(event) {
-    let uploader = document.querySelector(".form__uploader-section__sub-info")
     const file = event.target.files[0];
     const maxSize = 0.5 * 1024 * 1024; // 500ko
 
     if (file && file.size < maxSize) {
         uploader.innerText = "Upload your photo (JPG or PNG, max size: 500KB)."
         uploader.style.color = "white"
+        subInfo.classList.remove("content")
         const reader = new FileReader();
         reader.onload = function(e) {
             const img = document.querySelector('.input-cover');
@@ -60,55 +66,29 @@ document.getElementById('uploader').addEventListener('change', function(event) {
             localStorage.setItem("datasrc", src)
         };
         reader.readAsDataURL(file); // Lit le fichier comme une URL de données (base64)
-    }
-    else {
+    } else {
         uploader.innerText = "File too large. Please upload a photo under 500ko"
         uploader.style.color = "hsl(7, 71%, 60%)"
+        subInfo.classList.add("content")
     }
 });
 
-
-
 document.getElementById("name").addEventListener("input", (event) => {
-    if (!regexName.test(event.target.value)) {
-        event.target.style.borderColor = "hsl(7, 71%, 60%)"
-        let errorSpan = document.querySelector("#name + span")
-        errorSpan.style.display = "inline-block"
-    }
+    manageError(regexName, event.target, nameErrorSpan)
 })
 document.getElementById("email").addEventListener("input", (event) => {
-    if (!regexEmail.test(event.target.value)) {
-        event.target.style.borderColor = "hsl(7, 71%, 60%)"
-        let errorSpan = document.querySelector("#email + span")
-        errorSpan.style.display = "inline-block"
-    }
+    manageError(regexEmail, event.target, emailErrorSpan)
 })
 document.getElementById("github").addEventListener("input", (event) => {
-    if (!regexGithub.test(event.target.value)) {
-        event.target.style.borderColor = "hsl(7, 71%, 60%)"
-        let errorSpan = document.querySelector("#github + span")
-        errorSpan.style.display = "inline-block"
-    }
+    manageError(regexGithub, event.target, githubErrorSpan)
 })
 
-document.getElementById("name").addEventListener("input", (event) => {
-    if (regexName.test(event.target.value)) {
-        event.target.style.borderColor = "hsl(252, 6%, 83%)"
-        let errorSpan = document.querySelector("#name + span")
+function manageError(rule, targetElement, errorSpan) {
+    if (!rule.test(targetElement.value)) {
+        targetElement.style.borderColor = "hsl(7, 71%, 60%)"
+        errorSpan.style.display = "inline-block"
+    } else {
+        targetElement.style.borderColor = "hsl(252, 6%, 83%)"
         errorSpan.style.display = "none"
     }
-})
-document.getElementById("email").addEventListener("input", (event) => {
-    if (regexEmail.test(event.target.value)) {
-        event.target.style.borderColor = "hsl(252, 6%, 83%)"
-        let errorSpan = document.querySelector("#email + span")
-        errorSpan.style.display = "none"
-    }
-})
-document.getElementById("github").addEventListener("input", (event) => {
-    if (regexGithub.test(event.target.value)) {
-        event.target.style.borderColor = "hsl(252, 6%, 83%)"
-        let errorSpan = document.querySelector("#github + span")
-        errorSpan.style.display = "none"
-    }
-})
+}
